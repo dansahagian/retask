7ba84c3e9816ec3b09951edcf9274e4b704fc2af
Dan Sahagian <dan@dasa.dev>
2023-09-16 13:58:56 -0700

    Rename to tasks and remove requirements

--- main.py
+++ main.py
@@ -1,74 +1,68 @@
-from datetime import date
-from enum import Enum
+from dataclasses import dataclass
+from datetime import date, datetime
 from pathlib import Path
 from typing import Tuple, List
 
-from pydantic import BaseModel
-
-from config import PATH_TO_DAILY_DIR, TODOS
+from config import PATH_TO_DAILY_DIR, TASKS
 
 
-class IntervalType(str, Enum):
-    DAY = "d"
-    WEEK = "w"
-    MONTH = "m"
-
-
-class Todo(BaseModel):
+@dataclass
+class Task:
     start_date: date
     interval_num: int
-    interval_type: IntervalType
+    interval_type: str
     description: str
 
 
-def is_due_today(today: date, todo: Todo) -> bool:
-    delta_days = (today - todo.start_date).days
+def is_due_today(today: date, task: Task) -> bool:
+    delta_days = (today - task.start_date).days
 
     if delta_days < 0:
         return False
-    if todo.interval_type == "w":
+    if task.interval_type == "w":
         return delta_days % 7 == 0
-    if todo.interval_type == "m" and today.day == todo.start_date.day:
+    if task.interval_type == "m" and today.day == task.start_date.day:
         return True
     return delta_days == 0
 
 
-def generate_todos(today: date, todos: Tuple[Tuple[str, int, str, str]]) -> List[Todo]:
+def generate_tasks(today: date, tasks: Tuple[Tuple[str, int, str, str]]) -> List[Task]:
     output = []
-    for t in todos:
+    for t in tasks:
         start_date, interval_num, interval_type, description = t
-        todo = Todo(
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        task = Task(
             start_date=start_date,
             interval_num=interval_num,
             interval_type=interval_type,
             description=description,
         )
 
-        if is_due_today(today, todo):
-            output.append(todo)
+        if is_due_today(today, task):
+            output.append(task)
 
     return output
 
 
-def write_todos(path: Path, todos: List[Todo]):
+def write_tasks(path: Path, tasks: List[Task]):
     if path.exists():
         with open(path, "a") as f:
             f.write("\n## Tasks\n")
-            for todo in todos:
-                f.write(f"- [ ] {todo.description}\n")
+            for task in tasks:
+                f.write(f"- [ ] {task.description}\n")
     else:
         with open(path, "w") as f:
             f.write("\n## Tasks\n")
-            for todo in todos:
-                f.write(f"- [ ] {todo.description}\n")
+            for task in tasks:
+                f.write(f"- [ ] {task.description}\n")
 
 
 def main():
     today = date.today()
     path = Path(PATH_TO_DAILY_DIR, f"{today.strftime('%Y-%m-%d')}.md")
 
-    todos = generate_todos(today, TODOS)
-    write_todos(path, todos)
+    tasks = generate_tasks(today, TASKS)
+    write_tasks(path, tasks)
 
 
 if __name__ == "__main__":
--- template_config.py
+++ template_config.py
@@ -1,7 +1,8 @@
 PATH_TO_DAILY_DIR = "/Users/dan/d/notes/Personal/private/daily/"
-TODOS = (
+TASKS = (
     ("2023-09-16",   1, "d", "Take Zyrtec"),
     ("2023-09-17",   2, "w", "Water Desk Plants"),
     ("2023-09-17",   2, "w", "Trash & Recycle"),
     ("2023-09-18",   1, "w", "Complete Backups"),
+    ("2023-09-20",   0, "m", "Do something every month on the 20th")
 )
